bd43d1ea95a78a12a70c360ad2e3d6e2b894bb0a
jufa2401 <jufa2401@gmail.com>
2017-09-28 13:30:10 +0200

    Hvordan det fungerer og hvorfor:
    https://appsandbiscuits.com/listview-tutorial-android-12-ccef4ead27cc

--- app/src/main/java/com/example/s165158/aspiri/ClickOnList.java
+++ app/src/main/java/com/example/s165158/aspiri/ClickOnList.java
@@ -3,7 +3,7 @@ package com.example.s165158.aspiri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
-
+//Denne klasse håndterer den activity man ryger ind i når man klikker på et list-objekt
 public class ClickOnList extends AppCompatActivity {
 
     @Override
