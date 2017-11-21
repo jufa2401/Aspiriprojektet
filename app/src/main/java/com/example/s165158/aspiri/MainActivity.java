868a760cf9ccdbe6d3c31889c9e43f9c7ffb02f2
Jonas mIkkelsen <mikkelsen.v.jonas@gmail.com>
2017-11-21 20:05:09 +0100

    påbegyndte expandable list i drawer

--- app/src/main/java/com/example/s165158/aspiri/MainActivity.java
+++ app/src/main/java/com/example/s165158/aspiri/MainActivity.java
@@ -14,7 +14,21 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ExpandableListView;
 import android.widget.Toast;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ExpandableListView;
+import android.widget.ExpandableListView.OnChildClickListener;
+import android.widget.ExpandableListView.OnGroupClickListener;
+import android.widget.ExpandableListView.OnGroupCollapseListener;
+import android.widget.ExpandableListView.OnGroupExpandListener;
+import android.widget.Toast;
+
 
 import com.example.s165158.aspiri.test.TestFlipcard;
 import com.example.s165158.aspiri.test2.ListFragment;
@@ -25,20 +39,22 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     ListFragment listFragment;
     Toolbar toolbar;
 
+    ExpandableListAdapter listAdapter;
+    ExpandableListView expandableListView;
+    List<String> listDataHeader;
+    HashMap<String, List<String>> listDataChild;
+
     //On createmetode
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
 
-
         // Fanebladet
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
-
 
         //Drawer menu
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -49,6 +65,17 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
+        // get the listview
+        //expandableListView = (ExpandableListView) findViewById(R.id.lvExp);
+
+        // preparing list data
+        //prepareListData();
+
+        //Creating the list adapter from class
+        //listAdapter = new ExpandableListAdapter(getApplicationContext(), listDataHeader, listDataChild);
+
+        // setting list adapter
+        //expandableListView.setAdapter(listAdapter);
 
     }
     //    Tredottede menu initialiseres her
@@ -57,6 +84,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         getMenuInflater().inflate(R.menu.three_dot_menu, menu);
         return true;
     }
+
 
     //    Onclick for tredottede menu
     @Override
@@ -76,8 +104,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 // User chose the "Settings" item, show the app settings UI...
                 Toast.makeText(getApplicationContext(), "Yet to be implemented", Toast.LENGTH_SHORT).show();
                 Log.d("AspiriApp", "action_settings pressed");
-
-
                 return true;
 
             case R.id.three_dot_favorite:
@@ -93,7 +119,8 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         }
     }
 
-    // On click for de forskellige optioner i hamburgernavigation menuen.
+
+    // On click for de forskellige optioner i drawer.
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()) {
@@ -106,7 +133,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
             case R.id.drawer_store:
                 Intent goToStore = new Intent(Intent.ACTION_VIEW);
-                goToStore.setData(Uri.parse(getString(R.string.drawer_store_url)));
+                goToStore.setData(Uri.parse(getString(R.string.drawer_URL_ToTheStore)));
                 startActivity(goToStore);
                 Log.d("AspiriApp","Webstore pressed");
                 return true;
@@ -128,25 +155,25 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
             //        Maps Navigation
             case R.id.drawer_Kbh:
-                Intent goToSigurdsGade = new Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.drawer_kbh_to_maps)));
+                Intent goToSigurdsGade = new Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.drawer_URL_KBH)));
                 startActivity(goToSigurdsGade);
                 Log.d("AspiriApp", "drawer_KBH pressed");
                 return true;
 
             case R.id.drawer_Aarhus:
-                Intent goToAarhus = new Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.drawer_Aarhus_to_maps)));
+                Intent goToAarhus = new Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.drawer_URL_Aarhus)));
                 startActivity(goToAarhus);
                 Log.d("AspiriApp", "drawer_Aarhus pressed");
                 return true;
 
             case R.id.drawer_Odense:
-                Intent goToOdense = new Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.drawer_Odense_to_maps)));
+                Intent goToOdense = new Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.drawer_URL_Odense)));
                 startActivity(goToOdense);
                 Log.d("AspiriApp", "drawer_Odense pressed");
                 return true;
 
             case R.id.drawer_HQ:
-                Intent goToFalonerAlle = new Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.drawer_HQ_to_maps)));
+                Intent goToFalonerAlle = new Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.drawer_URL_HQ)));
                 startActivity(goToFalonerAlle);
                 Log.d("AspiriApp", "drawer_HQ pressed");
                 return true;
@@ -165,11 +192,13 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
+
     @Override
     public void onResume() {
         super.onResume();
         listFragment = new ListFragment();
-// Insert the fragment by replacing any existing fragment
+        // Insert the fragment by replacing any existing fragment
         FragmentManager fragmentManager = getFragmentManager();
         fragmentManager.beginTransaction()
                 .replace(R.id.fragmentindhold, listFragment)
@@ -185,6 +214,54 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         } else {
             getFragmentManager().popBackStack();
         }
+    }
+
+    private void prepareListData() {
+        listDataHeader = new ArrayList<String>();
+        listDataChild = new HashMap<String, List<String>>();
+
+        // Adding child data
+        listDataHeader.add("Top 250");
+        listDataHeader.add("Now Showing");
+        listDataHeader.add("Coming Soon..");
+        listDataHeader.add("Testeri");
+
+        // Adding child data
+        List<String> top250 = new ArrayList<String>();
+        top250.add("The Shawshank Redemption");
+        top250.add("The Godfather");
+        top250.add("The Godfather: Part II");
+        top250.add("Pulp Fiction");
+        top250.add("The Good, the Bad and the Ugly");
+        top250.add("The Dark Knight");
+        top250.add("12 Angry Men");
+
+        List<String> nowShowing = new ArrayList<String>();
+        nowShowing.add("The Conjuring");
+        nowShowing.add("Despicable Me 2");
+        nowShowing.add("Turbo");
+        nowShowing.add("Grown Ups 2");
+        nowShowing.add("Red 2");
+        nowShowing.add("The Wolverine");
+
+        List<String> comingSoon = new ArrayList<String>();
+        comingSoon.add("2 Guns");
+        comingSoon.add("The Smurfs 2");
+        comingSoon.add("The Spectacular Now");
+        comingSoon.add("The Canyons");
+        comingSoon.add("Europa Report");
+
+        List<String> Testeri = new ArrayList<String>();
+        Testeri.add("noget 1");
+        Testeri.add("noget 2");
+        Testeri.add("noget 3");
+        Testeri.add("noget 4");
+
+        // Header, Child data
+        listDataChild.put(listDataHeader.get(0), top250);
+        listDataChild.put(listDataHeader.get(1), nowShowing);
+        listDataChild.put(listDataHeader.get(2), comingSoon);
+        listDataChild.put(listDataHeader.get(3), Testeri);
     }
 }
 
