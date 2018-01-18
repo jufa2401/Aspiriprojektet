fec5414d70bc1a66c46eeea55aced27cee8ad450
jufa2401 <jufa2401@gmail.com>
2018-01-18 17:28:54 +0100

    final push merging of flipcard_dev_merge and master, fix LateX spelling

--- app/src/main/java/com/aspiri/karakterloeft/games/quiz/QuestionBank.java
+++ app/src/main/java/com/aspiri/karakterloeft/games/quiz/QuestionBank.java
@@ -48,25 +48,32 @@ public class QuestionBank {
         myDataBaseHelper = new ourDatabaseHelper(context);
         list = myDataBaseHelper.getAllQuestionsList();//get questions/choices/answers from database
 
+        String[] db1 = new String[]{"$$ \\pi \\cdot r^{2}$$", "$$ \\pi \\cdot 2 \\cdot r $$", "$$ \\pi \\cdot 4\\cdot r $$", "$$\\pi \\cdot 2 \\cdot r $$"};
+        String[] db2 = new String[]{"$$ \\sqrt(\\frac{a}{\\pi} $$", "$$ \\frac{d}{2} $$", "$$ \\pi \\cdot 2\\cdot r $$", "$$ h \\cdot g $$"};
+        String[] db3 = new String[]{"Størrelsen af cirklen", "Arealet af den indskrevne firkant", "Længden på linjen gennem centrum, afgrænset af cirkelperiferien", "Radius ganget med Pi"};
+        String[] db4 = new String[]{"$$ 14,67 $$", "$$ \\pi $$", "$$ 3,1499 $$", "$$ 1 $$"};
+        String[] db5 = new String[]{"$$ 2\\cdot 3 $$", "$$ 2\\cdot r \\cdot \\pi $$", "$$ \\frac{a}{\\pi} $$", "$$ d \\cdot \\pi $$"};
+        String[] db6 = new String[]{"En linje som går igennem en cirkel", "Netop den linje som går igennem centrum af cirklen", "Højden af en cirkel", "En linje som er afgrænset af cirklens periferi, og rører denne 2 gange"};
+        String[] db7 = new String[]{"$$ 2\\cdot d \\cdot (sin(\\frac{v}{2})) $$", "$$ 2\\cdot r \\cdot (sin(\\frac{v}{2}))\\ $$", "$$ 2 \\cdot r \\cdot (cos(\\frac{v}{2})) $$", "$$ 4 \\cdot d \\cdot sin(\\frac{v}{2})) $$"};
+        String[] db8 = new String[]{"To piger", "Tre mændt", "Femten drenge", "det kan man"};
         if (list.isEmpty()) {//if list is empty, populate database with default questions/choices/answers
 
             myDataBaseHelper.addInitialQuestion(new Question("Hvad er areal for en cirkel?",
-                    new String[]{"$$ \\pi \\cdot r^{2}$$", "$$ \\pi \\cdot 2 \\cdot r $$", "$$ \\pi \\cdot 4\\cdot r $$", "$$\\pi \\cdot 2 \\cdot r $$"}
-                    , "$$ \\pi \\cdot r^{2}$$"));
+                    db1, db1[0]));
             myDataBaseHelper.addInitialQuestion(new Question("Hvad er radius for en cirkel?",
-                    new String[]{"$$ \\sqrt(\\frac{a}{\\pi} $$", "$$ \\frac{d}{2} $$", "$$ \\pi \\cdot 2\\cdot r $$", "$$ h \\cdot g $$"}, "$ \\sqrt(\\frac{a}{\\pi} $$"));
+                    db2, db2[0]));
             myDataBaseHelper.addInitialQuestion(new Question("Hvad er diameteren et udtryk for?",
-                    new String[]{"Størrelsen af cirklen", "Arealet af den indskrevne firkant", "Længden på linjen gennem centrum, afgrænset af cirkelperiferien", "Radius ganget med Pi"}, "Længden på linjen gennem centrum, afgrænset af cirkelperiferien"));
+                    db3, db3[2]));
             myDataBaseHelper.addInitialQuestion(new Question("Hvad er arealet for en cirkel med radius på 1",
-                    new String[]{"$$ 14,67 $$", "$$ \\pi $$", "$$ 3,1499 $$", "$$ 1 $$"}, "$$ \\pi $$"));
+                    db4, db4[1]));
             myDataBaseHelper.addInitialQuestion(new Question("Hvad er formlen for en omkreds af en cirkel",
-                    new String[]{"$$ 2\\cdot \\3 $$", "$$ 2\\cdot r \\cdot \\pi $$", "$$ \\frac{a}{\\pi} $$", "$$ d \\cdot \\pi $$"}, "$$ d \\cdot \\pi $$"));
+                    db5, db5[3]));
             myDataBaseHelper.addInitialQuestion(new Question("Hvad er en korde?",
-                    new String[]{"En linje som går igennem en cirkel", "Netop den linje som går igennem centrum af cirklen", "Højden af en cirkel", "En linje som er afgrænset af cirklens periferi, og rører denne 2 gange"}, "En linje som er afgrænset af cirklens periferi, og rører denne 2 gange"));
+                    db6, db6[3]));
             myDataBaseHelper.addInitialQuestion(new Question("Hvad er en formlen for en korde?",
-                    new String[]{"$$ 2\\cdot d \\cdpt (sin(\\frac{v}{2})) $$", "$$ 2\\cdot r \\cdot (sin(\\frac{v}{2}))\\ $$", "$$ 2 \\cdot r \\cdot (cos(\\frac{v}{2})) $$", "$$ 4 \\cdot d \\cdot sin(\\frac{v}{2})) $$"}, "$$ 2\\cdot r \\cdot (sin(\\frac{v}{2}))\\ $$"));
+                    db7, db7[2]));
             myDataBaseHelper.addInitialQuestion(new Question("Hvordan kan man huske hvad arealet for en cirkel er?",
-                    new String[]{"To piger", "Tre mændt", "Femten drenge", "det kan man"}, "$$ 2\\cdot r \\cdot (sin(\\frac{v}{2}))\\ $$"));
+                    db8, db8[0]));
 
             list = myDataBaseHelper.getAllQuestionsList();//get list from database again
 
